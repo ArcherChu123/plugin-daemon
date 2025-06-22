e06565c4be22423b3d58e8a2da17f12e0f77bcb2
Archer Chu <archerchu@gmail.com>
2025-06-22 17:22:36 +0800

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,37 +1,29 @@
 package main
 
 import (
-	"log"
-	// "github.com/ArcherChu123/plugin-daemon/internal/server"
-	// "github.com/ArcherChu123/plugin-daemon/internal/types/app"
-	// "github.com/ArcherChu123/plugin-daemon/internal/utils/log"
-	// "github.com/joho/godotenv"
-	// "github.com/kelseyhightower/envconfig"
-	"github.com/ArcherChu123/plugin-daemon/core/config"
+	"github.com/ArcherChu123/plugin-daemon/internal/server"
+	"github.com/ArcherChu123/plugin-daemon/internal/types/app"
+	"github.com/ArcherChu123/plugin-daemon/internal/utils/log"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
 )
 
 func main() {
+	var config app.Config
 
-	err := config.LoadFromEnv()
+	// load env
+	godotenv.Load()
+
+	err := envconfig.Process("", &config)
 	if err != nil {
-		log.Fatalf("加载配置失败: %v", err)
+		log.Panic("Error processing environment variables: %s", err.Error())
 	}
 
-	// var config app.Config
+	config.SetDefault()
 
-	// // load env
-	// godotenv.Load()
+	if err := config.Validate(); err != nil {
+		log.Panic("Invalid configuration: %s", err.Error())
+	}
 
-	// err := envconfig.Process("", &config)
-	// if err != nil {
-	// 	log.Panic("Error processing environment variables: %s", err.Error())
-	// }
-
-	// config.SetDefault()
-
-	// if err := config.Validate(); err != nil {
-	// 	log.Panic("Invalid configuration: %s", err.Error())
-	// }
-
-	// (&server.App{}).Run(&config)
+	(&server.App{}).Run(&config)
 }
